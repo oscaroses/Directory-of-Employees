dfb98ca3987a111a50e6f17992cee73c3d3a2c6e
PeeWee <oscar.oses.1127@gmail.com>
2020-10-30 21:42:00 -0400

    added and delted stuff. starting over.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import EmployeeTable from './components/EmployeeTable';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <EmployeeTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="Header">
+            <h1>Directory of Employees</h1>
+        </div>
+    )
+}
+
+export default Header;
